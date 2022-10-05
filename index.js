7c2069d4f3be054c41961e30fec5872e13831983
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2022-10-05 15:07:09 +0530

    index.js done

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var output = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
 function clickHandler() {
-    console.log(txtInput.value);
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText;
+        })
+        .catch(errorHandler)
 }
 
 btnTranslate.addEventListener('click', clickHandler);
